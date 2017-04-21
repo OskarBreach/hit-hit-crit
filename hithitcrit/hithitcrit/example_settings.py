04fe9926ec128e20008414dd2094384e0e9df92e
Oskar Breach <oskar.breach@gmail.com>
2017-04-21 16:57:15 +0100

    Change database to PostgreSQL

--- hithitcrit/hithitcrit/example_settings.py
+++ hithitcrit/hithitcrit/example_settings.py
@@ -1,2 +1,17 @@
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'Sample key, replace with secure key in production!'
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hit_hit_crit',
+        'USER': 'db user',
+        'PASSWORD': 'db password',
+        'HOST': 'db host',
+        'PORT': 'db port',
+    }
+}
+
