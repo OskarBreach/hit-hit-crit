dc6d135565314266245596edf6ad70b39f41b4d4
Oskar Breach <oskar.breach@gmail.com>
2017-04-21 17:59:16 +0100

    Source data from Guido Kessels' xwing-data

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # hit-hit-crit
 Squad builder for Fantasy Flight's X-Wing Miniatures Game
+
+## Data
+
+Data sourced from Guido Kessels' [xwing-data](https://github.com/guidokessels/xwing-data).
+
+## Licence
+
+MIT
--- hithitcrit/hithitcrit/settings.py
+++ hithitcrit/hithitcrit/settings.py
@@ -15,6 +15,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Path to xwing-data files
+XWING_DATA_DIR = os.path.join(BASE_DIR, 'xwingdata/xwing-data/')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
