50bf39ddb49996959e6ff212c84d578d0022a472
Oskar Breach <oskar.breach@gmail.com>
2017-04-21 17:00:19 +0100

    Add directory to serve media from

--- hithitcrit/hithitcrit/example_settings.py
+++ hithitcrit/hithitcrit/example_settings.py
@@ -15,3 +15,4 @@ DATABASES = {
     }
 }
 
+MEDIA_ROOT = '/var/www/hithitcrit.co.uk/media/'
--- hithitcrit/hithitcrit/settings.py
+++ hithitcrit/hithitcrit/settings.py
@@ -104,6 +104,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 # Import local settings
 
