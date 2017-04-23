c56edadfd6e85c9f03b53dd370948c604a9e972b
Oskar Breach <oskar.breach@gmail.com>
2017-04-23 19:05:06 +0100

    Name url capture groups

--- hithitcrit/viewer/urls.py
+++ hithitcrit/viewer/urls.py
@@ -5,12 +5,12 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?i)pilots/$', views.pilots, name='pilots'),
-    url(r'^(?i)pilots/(\d+)/$', views.pilot_by_id, name='pilot-by-id'),
+    url(r'^(?i)pilots/(?P<id>\d+)/$', views.pilot_by_id, name='pilot-by-id'),
     url(r'^(?i)pilots/ship/(?P<ship>[-\w]+)/$', views.pilots_by_ship, name='pilots-by-ship'),
     url(r'^(?i)upgrades/$', views.upgrades, name='upgrades'),
-    url(r'^(?i)upgrades/(\d+)/$', views.upgrade_by_id, name='upgrade-by-id'),
+    url(r'^(?i)upgrades/(?P<id>\d+)/$', views.upgrade_by_id, name='upgrade-by-id'),
     url(r'^(?i)reference-cards/$', views.reference_cards, name='reference-cards'),
-    url(r'^(?i)reference-cards/(\d+)/$', views.reference_card_by_id, name='reference-card-by-id'),
+    url(r'^(?i)reference-cards/(?P<id>\d+)/$', views.reference_card_by_id, name='reference-card-by-id'),
     url(r'^(?i)conditions/$', views.conditions, name='conditions'),
-    url(r'^(?i)conditions/(\d+)/$', views.condition_by_id, name='condition-by-id'),
+    url(r'^(?i)conditions/(?P<id>\d+)/$', views.condition_by_id, name='condition-by-id'),
 ]
