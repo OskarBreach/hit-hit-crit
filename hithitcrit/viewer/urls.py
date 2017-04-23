b2934d1e12103b030b1a4a997d06bb30d94390bc
Oskar Breach <oskar.breach@gmail.com>
2017-04-23 16:10:08 +0100

    Replace all absolute hyperlinks with lookups

--- hithitcrit/viewer/urls.py
+++ hithitcrit/viewer/urls.py
@@ -4,12 +4,12 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^pilots/$', views.pilots),
-    url(r'^pilots/(\d+)/$', views.pilot_by_id),
-    url(r'^upgrades/$', views.upgrades),
-    url(r'^upgrades/(\d+)/$', views.upgrade_by_id),
-    url(r'^reference-cards/$', views.reference_cards),
-    url(r'^reference-cards/(\d+)/$', views.reference_card_by_id),
-    url(r'^conditions/$', views.conditions),
-    url(r'^conditions/(\d+)/$', views.condition_by_id),
+    url(r'^pilots/$', views.pilots, name='pilots'),
+    url(r'^pilots/(\d+)/$', views.pilot_by_id, name='pilot-by-id'),
+    url(r'^upgrades/$', views.upgrades, name='upgrades'),
+    url(r'^upgrades/(\d+)/$', views.upgrade_by_id, name='upgrade-by-id'),
+    url(r'^reference-cards/$', views.reference_cards, name='reference-cards'),
+    url(r'^reference-cards/(\d+)/$', views.reference_card_by_id, name='reference-card-by-id'),
+    url(r'^conditions/$', views.conditions, name='conditions'),
+    url(r'^conditions/(\d+)/$', views.condition_by_id, name='condition-by-id'),
 ]
